43284a1813a0f6b69c132d2047950d142942f1b4
Eric Dill <edill@bnl.gov>
2015-10-29 16:02:05 -0400

    ENH: Add lazy chxtools handler

---
+++ chxtools/handlers.py
@@ -0,0 +1,54 @@
+from filestore.retrieve import HandlerBase
+from .pims_readers import EigerImages
+
+EIGER_MD_DICT = {
+    'y_pixel_size': 'entry/instrument/detector/y_pixel_size',
+    'x_pixel_size': 'entry/instrument/detector/x_pixel_size',
+    'detector_distance': 'entry/instrument/detector/detector_distance',
+    'incident_wavelength': 'entry/instrument/beam/incident_wavelength',
+    'frame_time': 'entry/instrument/detector/frame_time',
+    'beam_center_x': 'entry/instrument/detector/beam_center_x',
+    'beam_center_y': 'entry/instrument/detector/beam_center_y',
+    'count_time': 'entry/instrument/detector/count_time'
+}
+
+class FixedEigerImages(EigerImages):
+    def __init__(self, path, metadata):
+        super().__init__(path)
+        self._metadata = metadata
+    
+    @property
+    def md(self):
+        return self._metadata
+    
+    @property
+    def dtype(self):
+        return self.pixel_type
+    
+    @property
+    def shape(self):
+        return self.frame_shape
+
+class LazyEigerHandler(HandlerBase):
+    specs = {'AD_EIGER'} | HandlerBase.specs
+    def __init__(self, fpath, frame_per_point, mapping=None):
+        # create pims handler
+        self.vals_dict = EIGER_MD_DICT.copy()
+        if mapping is not None:
+            self.vals_dict.update(mapping)
+        self._base_path = fpath
+        self.fpp = frame_per_point
+
+    def __call__(self, seq_id):
+        import h5py
+        master_path = '{}_{}_master.h5'.format(self._base_path, seq_id)
+        md = {}
+        print('hdf5 path = %s' % master_path)
+        with h5py.File(master_path, 'r') as f:
+            md = {k: f[v].value for k, v in self.vals_dict.items()}
+            
+        # TODO Return a multi-dimensional PIMS seq.
+        return FixedEigerImages(master_path, md)
+
+deregister_handler('AD_EIGER')
+register_handler('AD_EIGER', LazyEigerHandler)
