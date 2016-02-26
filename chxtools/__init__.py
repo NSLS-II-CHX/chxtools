d6ec28c1cc943092a8dd5f7ba9c051e01aace664
xf11id <dchabot@bnl.gov>
2016-02-26 17:41:32 -0500

    MNT: Add versioneer

--- chxtools/__init__.py
+++ chxtools/__init__.py
@@ -1,1 +1,5 @@
 __author__ = 'edill'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from __future__ import (absolute_import, division, print_function)
 
 import sys
 import warnings
-
+import versioneer
 
 from setuptools import setup, find_packages
 
 setup(
     name='chxtools',
-    version='0.0.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author='Brookhaven National Laboratory',
     packages=find_packages(),
     package_data={'chxtools/X-ray_database' : ['.dat']},
