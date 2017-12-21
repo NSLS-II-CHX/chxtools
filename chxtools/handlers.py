02efd1806e6ee986adcb3fdd141fe92a38ef4b45
Julien R. Lhermitte <lhermitte@bnl.gov>
2017-12-21 16:01:29 -0500

    condensed handlers

--- chxtools/handlers.py
+++ chxtools/handlers.py
@@ -1,68 +1,14 @@
+# this does handler registration?
 from databroker import Broker
 from databroker.assets.handlers_base import HandlerBase
-from chxtools.pims_readers.eiger import EigerImages
+#from chxtools.pims_readers.eiger import EigerImages
+from eiger_io.fs_handlers_dask import EigerDaskHandler
+from eiger_io.fs_handlers import EigerHandler
 
-EIGER_MD_DICT = {
-    'y_pixel_size': 'entry/instrument/detector/y_pixel_size',
-    'x_pixel_size': 'entry/instrument/detector/x_pixel_size',
-    'detector_distance': 'entry/instrument/detector/detector_distance',
-    'incident_wavelength': 'entry/instrument/beam/incident_wavelength',
-    'frame_time': 'entry/instrument/detector/frame_time',
-    'beam_center_x': 'entry/instrument/detector/beam_center_x',
-    'beam_center_y': 'entry/instrument/detector/beam_center_y',
-    'count_time': 'entry/instrument/detector/count_time',
-    'pixel_mask': 'entry/instrument/detector/detectorSpecific/pixel_mask',
-}
+# toggle use of dask or no dask
+EigerHandler = EigerDaskHandler
 
-class FixedEigerImages(EigerImages):
-    def __init__(self, path, metadata):
-        super().__init__(path)
-        self._metadata = metadata
 
-    @property
-    def md(self):
-        return self._metadata
-
-    @property
-    def dtype(self):
-        return self.pixel_type
-
-    @property
-    def shape(self):
-        return self.frame_shape
-
-class LazyEigerHandler(HandlerBase):
-    specs = {'AD_EIGER'} | HandlerBase.specs
-    def __init__(self, fpath, frame_per_point, mapping=None):
-        # create pims handler
-        self.vals_dict = EIGER_MD_DICT.copy()
-        if mapping is not None:
-            self.vals_dict.update(mapping)
-        self._base_path = fpath
-        self.fpp = frame_per_point
-
-    def __call__(self, seq_id):
-        import h5py
-        master_path = '{}_{}_master.h5'.format(self._base_path, seq_id)
-        md = {}
-        #print('hdf5 path = %s' % master_path)
-        with h5py.File(master_path, 'r') as f:
-            md = {k: f[v].value for k, v in self.vals_dict.items()}
-        # the pixel mask from the eiger contains:
-        # 1  -- gap
-        # 2  -- dead
-        # 4  -- under-responsive
-        # 8  -- over-responsive
-        # 16 -- noisy
-        pixel_mask = md['pixel_mask']
-        pixel_mask[pixel_mask>0] = 1
-        pixel_mask[pixel_mask==0] = 2
-        pixel_mask[pixel_mask==1] = 0
-        pixel_mask[pixel_mask==2] = 1
-        md['framerate'] = 1./md['frame_time']
-        # TODO Return a multi-dimensional PIMS seq.
-        return FixedEigerImages(master_path, md)
-
-#db.fs.deregister_handler('AD_EIGER')
 db = Broker.named('chx')
-db.reg.register_handler('AD_EIGER', LazyEigerHandler)
+db.reg.register_handler('AD_EIGER2', EigerHandler)
+db.reg.register_handler('AD_EIGER2', EigerHandler)
