a2df92999719c8a6ca3e339a881b414b75044412
Julien R. Lhermitte <lhermitte@bnl.gov>
2017-12-21 16:47:13 -0500

    removed pims readers

--- chxtools/handlers.py
+++ chxtools/handlers.py
@@ -2,11 +2,14 @@
 from databroker import Broker
 from databroker.assets.handlers_base import HandlerBase
 #from chxtools.pims_readers.eiger import EigerImages
-from eiger_io.fs_handlers_dask import EigerDaskHandler
-from eiger_io.fs_handlers import EigerHandler
+from eiger_io.fs_handler_dask import EigerDaskHandler, EigerImagesDask
+from eiger_io.fs_handler import EigerHandler, EigerImages
 
 # toggle use of dask or no dask
-EigerHandler = EigerDaskHandler
+use_dask = True
+if use_dask:
+    EigerHandler = EigerDaskHandler
+    EigerImages = EigerImagesDask
 
 
 db = Broker.named('chx')
