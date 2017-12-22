43e779525baf28db551a357f7bdaee72daeea381
Julien R. Lhermitte <lhermitte@bnl.gov>
2017-12-21 20:41:03 -0500

    Renamed handlers

--- chxtools/handlers.py
+++ chxtools/handlers.py
@@ -1,16 +1,16 @@
-# this does handler registration?
+# handler registration and database instantiation should be done
+# here and only here!
 from databroker import Broker
 from databroker.assets.handlers_base import HandlerBase
 #from chxtools.pims_readers.eiger import EigerImages
-from eiger_io.fs_handler_dask import EigerDaskHandler, EigerImagesDask
+from eiger_io.fs_handler_dask import EigerHandlerDask, EigerImagesDask
 from eiger_io.fs_handler import EigerHandler, EigerImages
 
-print('here')
-
 # toggle use of dask or no dask
+# TODO : eventually choose one of the two
 use_dask = False
 if use_dask:
-    EigerHandler = EigerDaskHandler
+    EigerHandler = EigerHandlerDask
     EigerImages = EigerImagesDask
 
 
